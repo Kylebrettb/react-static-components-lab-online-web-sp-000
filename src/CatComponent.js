20de1ad7b301013c423f801628de8b0416b70248
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-09-25 19:54:28 +0000

    Automatically backed up by Learn

---
+++ src/CatComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class CatComponent extends Component {
+	render() {
+		return (
+			<div className="bar" id="cat">
+				<img src="/cat.gif" />
+			</div>
+		);
+	}
+}
+
+export default CatComponent;
